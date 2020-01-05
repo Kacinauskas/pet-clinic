e33be7bd10cf00bb45243b3186ce599253bafeb0
Laurynas Kacinauskas <kacinauskasl@gmail.com>
2020-01-05 19:45:55 +0200

    Updating OwnerController to list all owners on owners index page, closes #16

--- pet-clinic-web/src/main/java/com/keksas/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/keksas/petclinic/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package com.keksas.petclinic.controllers;
 
+import com.keksas.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping(value = "/owners")
-    public String owners() {
+    public String owners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
