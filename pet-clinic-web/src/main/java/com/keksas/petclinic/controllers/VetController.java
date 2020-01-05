b5a398fcde65186d1297e9ca0e852f5dc8441cd6
Laurynas Kacinauskas <kacinauskasl@gmail.com>
2020-01-05 20:00:13 +0200

    Updating VetController to list all vets on vet index page, closes #12

--- pet-clinic-web/src/main/java/com/keksas/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/keksas/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.keksas.petclinic.controllers;
 
+import com.keksas.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/vets")
 public class VetController {
 
-    @GetMapping(value = {"/vets "})
-    public String vets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @GetMapping()
+    public String vets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
